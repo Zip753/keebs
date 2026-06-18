af0c5366757f54e1126f32fecd489aabe8e2d8ea
Ivan Nikulin <ivan.nikulin@pactum.com>
2026-06-18 22:55:40 +0300

    Replace shifts on the main layer
    This replaces shifts with potentially more useful keys, forcing to use
    the spacebar shift, while also providing the bailout layer 1 which still
    has both shifts. QWERTY is effectively gone after this change.

--- th40/default/keymap.c
+++ th40/default/keymap.c
@@ -7,23 +7,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Layer 0: Colemak-DH base
     // - KC_O on Enter position: TH40 missing semicolon key = missing O in Colemak
     // - Enter: LSpace + RSpace (layer 2 right space)
-    // - DF(1): switch to QWERTY
     [0] = LAYOUT_tkl_ansi(
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,             KC_O,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RSFT,
-        KC_CAPS, KC_LALT, KC_LGUI,                   MO(2),   MO(3),   LSFT_T(KC_SPC),   KC_RGUI, KC_RALT, DF(1)
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    S(KC_SCLN), KC_BSPC,
+        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,                KC_O,
+        KC_LALT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_NO,   KC_K,    KC_H,    KC_COMM, KC_DOT,     KC_SLSH,
+        KC_CAPS, KC_LALT, KC_LGUI,                   MO(2),   MO(3),   LSFT_T(KC_SPC),   KC_RGUI, KC_RALT,    DF(1)
     ),
 
-    // Layer 1: QWERTY base (Mac)
-    // - ZXCV shift: slash moved between B and N (see DECISIONS.md)
-    // - KC_A on bottom-left: testing caps lock behavior (TODO: fix)
-    // - DF(0): switch back to Colemak layer
+    // same as layer 0, but with some bailout options:
+    // - shifts in their regular positions
+    // - / in on the middle key
     [1] = LAYOUT_tkl_ansi(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SLSH, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
-        KC_A,    KC_LALT, KC_LGUI,                   MO(2),   MO(3),   KC_SPC,           KC_RGUI, KC_RALT, DF(0)
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+        KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RSFT,
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, DF(0)
     ),
 
     // Layer 2: Symbols (hold left space)
