43dce6c46253c14849f35f87b5cfaf8849f9b416
Ivan Nikulin <ivan.nikulin@pactum.com>
2026-06-18 22:26:21 +0300

    Add mod-tap to the right spacebar
    This makes right spacebar to act as space on tap and as shift on hold.

--- th40/default/keymap.c
+++ th40/default/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
         KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,             KC_O,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_SLSH, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_RSFT,
-        KC_CAPS, KC_LALT, KC_LGUI,                   MO(2),   MO(3),   KC_SPC,           KC_RGUI, KC_RALT, DF(1)
+        KC_CAPS, KC_LALT, KC_LGUI,                   MO(2),   MO(3),   LSFT_T(KC_SPC),   KC_RGUI, KC_RALT, DF(1)
     ),
 
     // Layer 1: QWERTY base (Mac)
